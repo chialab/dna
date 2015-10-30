7d6754a56de9774a9c0b58458ab53a8f5271422d
Edoardo Cavazza <edoardo.cavazza@chialab.it>
2015-10-30 12:19:04 +0100

    release 0.3.0

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,7 +4,8 @@ require('load-grunt-tasks')(grunt);
 
 grunt.initConfig({
     clean: {
-        dist: ['dist'],
+        dist: ['dist/**/*'],
+        standalone: ['dist/**/*.js']
     },
     copy: {
         dist: {
@@ -18,19 +19,68 @@ grunt.initConfig({
     },
     babel: {
         options: {
-            sourceMap: true,
             modules: 'ignore'
         },
         dist: {
+            options: {
+                sourceMap: false,
+            },
+            files: [{
+                'dist/dna-components.js': 'dist/dna-components.js'
+            }]
+        }
+    },
+    vulcanize: {
+        options: {
+            inlineScripts: true,
+            inlineCss: true,
+            stripComments: true,
+        },
+        dist: {
             files: {
-                'dist/dna-component.next.js': 'src/dna-component.next.js',
-                'dist/dna-mixed-component.next.js': 'src/dna-mixed-component.next.js',
-                'dist/dna-event-component.next.js': 'src/dna-event-component.next.js',
-                'dist/dna-attributes-component.next.js': 'src/dna-attributes-component.next.js',
-                'dist/dna-base-component.next.js': 'src/dna-base-component.next.js',
+                'dist/dna-components.html': 'src/dna-components.html'
+            },
+        },
+        standalone: {
+            files: {
+                'dist/dna-components.html': 'dist/dna-components.html'
+            },
+        }
+    },
+    uglify: {
+        standalone: {
+            options: {
+                mangle: false,
+                sourceMap: false
+            },
+            files: {
+                'dist/dna-components.js': ['dist/dna-components.js']
+            }
+        }
+    },
+    crisper: {
+        dist: {
+            options: {
+                cleanup: false
+            },
+            files: {
+                'dist/dna-components.html': 'dist/dna-components.html'
+            }
+        }
+    },
+    replace: {
+        standalone: {
+            options: {
+                patterns: [{
+                    match: /import[^;|\n]+[;|\n]?/g,
+                    replacement: ''
+                }]
+            },
+            files: {
+                'dist/dna-components.js': 'dist/dna-components.js',
             }
         }
     }
 });
 
-grunt.registerTask('build', ['clean', 'copy', 'babel']);
+grunt.registerTask('build', ['clean', 'vulcanize:dist', 'crisper', 'replace', 'babel', 'uglify', 'vulcanize:standalone', 'clean:standalone']);
