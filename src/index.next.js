57c02cf7da612d4fb75ed5268bd30836fd8f9f38
Edoardo Cavazza <edoardo.cavazza@chialab.it>
2015-12-03 14:18:37 +0100

    add index.next.js

---
+++ src/index.next.js
@@ -0,0 +1,12 @@
+/**
+ * Copyright (c) 2015 Chialab s.rl.l. & Channelweb s.r.l.
+ * All rights reserved.
+ *
+ * Write your set of Web Components using <template>, ES2015 and (optionally) Sass.
+ */
+import './dna-api.next.js';
+import './dna-component.next.js';
+import './dna-mixed-component.next.js';
+import './dna-event-component.next.js';
+import './dna-attributes-component.next.js';
+import './dna-base-component.next.js';
