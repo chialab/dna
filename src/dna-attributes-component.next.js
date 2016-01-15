c4a6baff417b80e677110e4d6d878c08fd30771f
Edoardo Cavazza <edoardo.cavazza@chialab.it>
2016-01-15 09:50:16 +0100

    improve attributes/properties binding

--- src/dna-attributes-component.next.js
+++ src/dna-attributes-component.next.js
@@ -29,15 +29,52 @@ import { DNAComponent } from './dna-component.next.js'
  */
 export class DNAAttributesComponent extends DNAComponent {
 	/**
+     * Fires when an the element is registered.
+     */
+    static onRegister(...args) {
+        let attributesToWatch = this.attributes || [];
+		attributesToWatch.forEach((attr) => {
+			let descriptor = Object.getOwnPropertyDescriptor(this.prototype, attr) || {};
+			Object.defineProperty(this.prototype, attr, {
+				configurable: true,
+				get: descriptor.get || function () {
+					return this['__' + attr];
+				},
+				set: (function(descriptor) {
+					return function (value) {
+						let res;
+						if (descriptor.set) {
+							res = descriptor.set.call(this, value);
+						} else {
+							res = (this['__' + attr] = value);
+						}
+						if (value !== null && value !== undefined) {
+							this.setAttribute(attr, value);
+						} else {
+							this.removeAttribute(attr);
+						}
+						return res;
+					}
+				})(descriptor)
+			});
+		});
+    }
+	/**
 	 * On `created` callback, sync attributes with properties.
 	 */
 	createdCallback() {
 		DNAComponent.prototype.createdCallback.call(this);
-		var attributes = this.attributes || [];
+		let attributes = this.attributes || [];
         for (let i = 0, len = attributes.length; i < len; i++) {
             let attr = attributes[i];
             this.attributeChangedCallback(attr.name, undefined, attr.value);
         }
+		let ctrAttributes = this.constructor.attributes || [];
+		ctrAttributes.forEach((attr) => {
+			if (this[attr] !== null && this[attr] !== undefined) {
+				this.setAttribute(attr, this[attr]);
+			}
+		});
 	}
 	/**
 	 * On `attributeChanged` callback, sync attributes with properties.
@@ -48,7 +85,7 @@ export class DNAAttributesComponent extends DNAComponent {
 	 */
 	attributeChangedCallback(attr, oldVal, newVal) {
 		DNAComponent.prototype.attributeChangedCallback.call(this);
-		var cl = this.constructor;
+		let cl = this.constructor;
 		if (cl && cl.attributes && Array.isArray(cl.attributes)) {
 			if (cl.attributes.indexOf(attr) !== -1) {
 				this[attr] = newVal;
