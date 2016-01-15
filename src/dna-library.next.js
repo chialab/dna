b086d7ad69a3439ef832dd1c348d6128ae753a8f
Edoardo Cavazza <edoardo.cavazza@chialab.it>
2016-01-15 18:14:03 +0100

    imporve es5 library

--- src/dna-library.next.js
+++ src/dna-library.next.js
@@ -22,46 +22,14 @@ export function Create(tagName, config = {}) {
         newScope.prototype = scope;
         scope = newScope;
     }
-
-    let inheritPrototype = Object.create(DNABaseComponent.prototype, {
-        constructor: {
-            value: scope,
-            enumerable: false,
-            writable: true,
-            configurable: true
-        }
-    });
-
-    function bindFN(proto, k) {
-        return function() {
-            proto[k].apply(this, arguments);
-            DNABaseComponent.prototype[k].apply(this, arguments);
-        }
-    }
-
-    let proto = scope.prototype;
-    Object.setPrototypeOf(scope, DNABaseComponent);
-    scope.prototype = inheritPrototype;
-    for (var k in proto) {
+    let newScope = extend(scope, DNABaseComponent);
+    for (var k in scope.prototype) {
         if (['createdCallback', 'attachedCallback', 'detachedCallback', 'attributeChangedCallback'].indexOf(k) !== -1) {
-            scope.prototype[k] = bindFN(proto, k);
-        } else if (typeof proto[k] !== 'function') {
-            let descriptor = Object.getOwnPropertyDescriptor(proto, k) || {};
-            if (descriptor.get) {
-                Object.defineProperty(scope.prototype, k, {
-                    get: descriptor.get,
-                    set: descriptor.set,
-                    configurable: true
-                });
-            } else {
-                scope.prototype[k] = proto[k];
-            }
-        } else {
-            scope.prototype[k] = proto[k];
+            newScope.prototype[k] = bindFN(newScope.prototype[k], DNABaseComponent.prototype[k]);
         }
     }
-
-    Object.defineProperty(scope, 'tagName', {
+    Object.defineProperty(newScope, 'tagName', {
+        configurable: true,
         get: function () {
             return tagName
         }
@@ -69,7 +37,80 @@ export function Create(tagName, config = {}) {
 
     config.tagName = tagName;
 
-    return this.Register(scope, config);
+    var ctr = Register(newScope, config);
+
+    ctr.Extend = function (ctr2 = {}) {
+        let scope2 = (typeof ctr2 === 'function') ? ctr2 : (function(proto) { let fn = function() {}; fn.prototype = proto; return fn })(ctr2);
+        return extend(scope2, scope);
+    }
+
+    return ctr;
+}
+
+function getMethods (prototype) {
+    let res = [];
+    let added = ['name', 'length', 'prototype'];
+
+    function createProp(key) {
+        if (added.indexOf(key) === -1) {
+            let prop = {
+                key: key
+            }
+            if (typeof prototype[key] === 'function') {
+                prop['value'] = prototype[key];
+            } else {
+                let descriptor = Object.getOwnPropertyDescriptor(prototype, key) || {};
+                if (descriptor.get) {
+                    prop['get'] = descriptor.get;
+                    prop['set'] = descriptor.set;
+                } else {
+                    prop['value'] = prototype[key];
+                }
+            }
+            added.push(key)
+            return prop;
+        }
+    }
+
+    for (let key in prototype) {
+        let prop = createProp(key);
+        if (prop) {
+            res.push(prop);
+        }
+    }
+    let keys = Object.getOwnPropertyNames(prototype);
+    for (let k in keys) {
+        let prop = createProp(keys[k]);
+        if (prop) {
+            res.push(prop);
+        }
+    }
+    return res;
+}
+
+function extend (newClass, superClass) {
+    function ctr () {
+        babelHelpers.get(Object.getPrototypeOf(ctr.prototype), 'constructor', newClass).apply(newClass, arguments)
+    }
+    babelHelpers.inherits(ctr, superClass);
+    for (var k in superClass.prototype) {
+        let descriptor = Object.getOwnPropertyDescriptor(superClass.prototype, k) || {};
+        if (descriptor.get) {
+            Object.defineProperty(ctr.prototype, k, {
+                get: descriptor.get,
+                set: descriptor.set,
+                configurable: true
+            });
+        }
+    }
+    return babelHelpers.createClass(ctr, getMethods(newClass.prototype), getMethods(newClass));
+}
+
+function bindFN(protoFn, superFn) {
+    return function() {
+        protoFn.apply(this, arguments);
+        superFn.apply(this, arguments);
+    }
 }
 
 /**
