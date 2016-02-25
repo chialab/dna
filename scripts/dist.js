a4cf9c4570ba9e27012b703948d7d1e75905c9c4
Edoardo Cavazza <edoardo.cavazza@chialab.it>
2016-02-25 18:51:02 +0100

    chore: new dist script

--- scripts/dist.js
+++ scripts/dist.js
@@ -1,12 +1,16 @@
 var fs = require('fs'),
     path = require('path'),
-    Concat = require('concat-with-sourcemaps');
+    UglifyJS = require('uglify-js');
 
-var concat = new Concat(true, 'all.js', '\n');
 var cwd = process.cwd();
 
-concat.add('virtual-dom.js', fs.readFileSync(path.join(cwd, 'node_modules/virtual-dom/dist/virtual-dom.js'), 'utf8'));
-concat.add('dna-custom-elements.js', fs.readFileSync(path.join(cwd, 'node_modules/dna-polyfills/src/extra/custom-elements.js'), 'utf8'));
-concat.add('dna-components.js', fs.readFileSync(path.join(cwd, 'dist/dna-components.js'), 'utf8'), fs.readFileSync(path.join(cwd, 'dist/dna-components.js.map'), 'utf8'));
+var content = UglifyJS.minify([
+    path.join(cwd, 'node_modules/virtual-dom/dist/virtual-dom.js'),
+    path.join(cwd, 'node_modules/dna-polyfills/src/extra/custom-elements.js'),
+    path.join(cwd, 'dist/dna-components.js')
+], {
+    outSourceMap: 'dna-components.full.js.map'
+});
 
-fs.writeFileSync(path.join(cwd, 'dist/dna-components.full.js'), concat.content);
+fs.writeFileSync(path.join(cwd, 'dist/dna-components.full.js'), content.code);
+fs.writeFileSync(path.join(cwd, 'dist/dna-components.full.js.map'), content.map);
