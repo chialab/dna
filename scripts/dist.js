9e374aa6c6ba216c4e104caa951b4af1ce070616
Edoardo Cavazza <edoardo.cavazza@chialab.it>
2016-03-01 21:40:52 +0100

    chore: rename to `DNA`

--- scripts/dist.js
+++ scripts/dist.js
@@ -7,10 +7,10 @@ var cwd = process.cwd();
 var content = UglifyJS.minify([
     path.join(cwd, 'node_modules/virtual-dom/dist/virtual-dom.js'),
     path.join(cwd, 'node_modules/dna-polyfills/src/extra/custom-elements.js'),
-    path.join(cwd, 'dist/dna-components.js')
+    path.join(cwd, 'dist/dna.js')
 ], {
-    outSourceMap: 'dna-components.full.js.map'
+    outSourceMap: 'dna.full.js.map'
 });
 
-fs.writeFileSync(path.join(cwd, 'dist/dna-components.full.js'), content.code);
-fs.writeFileSync(path.join(cwd, 'dist/dna-components.full.js.map'), content.map);
+fs.writeFileSync(path.join(cwd, 'dist/dna.full.js'), content.code);
+fs.writeFileSync(path.join(cwd, 'dist/dna.full.js.map'), content.map);
