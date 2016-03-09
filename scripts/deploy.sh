49cd7c24d96fa07c8e1d7b668850feea7ef04c9b
Edoardo Cavazza <edoardo.cavazza@chialab.it>
2016-03-09 14:30:50 +0100

    chore: [ci] test and build to distribution repository

---
+++ scripts/deploy.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+TAG=$(git describe --tags)
+BUILD_DIR="./dist"
+REPO_DIR="./deploy"
+DIST_DIR="${REPO_DIR}/lib"
+REPO_URL="https://gitlab.com/dna-components/dna-library.git"
+TOKEN="214c8c5d-85b3-4732-b553-22c1585c4fee____"
+PACKAGE="dna-components"
+
+# build distribution scripts
+npm run build
+# clone distribution repo
+rm -rf $REPO_DIR
+git clone $REPO_URL $REPO_DIR
+# empty lib dir
+rm -rf $DIST_DIR
+# copy distribution files
+cp -R $BUILD_DIR $DIST_DIR
+# update distribution project
+cd $REPO_DIR
+sed -i "" "s/\"version\": \".*\"/\"version\": \"${TAG:1}\"/" package.json
+sed -i "" "s/\"version\": \".*\"/\"version\": \"${TAG:1}\"/" bower.json
+git add .
+git commit -m "release: ${TAG}"
+git tag -a ${TAG} -m "release: ${TAG}"
+git push
+cd ..
