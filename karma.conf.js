4dee3cb1366077d6a732204b19b96b21ba9a1c8d
Edoardo Cavazza <edoardo.cavazza@chialab.it>
2016-04-05 15:49:47 +0200

    test: fix karma conf

--- karma.conf.js
+++ karma.conf.js
@@ -7,4 +7,5 @@ module.exports = function(karma) {
         },
     });
     karma.files.unshift('node_modules/dna-polyfills/src/extra/custom-elements.js');
+    karma.files.push('node_modules/dom-delegate/lib/delegate.js');
 };
