4a0ea18d1e266e72bf770b036c1a0baf8025caf1
Edoardo Cavazza <edoardo.cavazza@chialab.it>
2016-04-30 19:54:30 +0200

    test: fix karma config

--- karma.conf.js
+++ karma.conf.js
@@ -17,6 +17,7 @@ module.exports = function(karma) {
         'node_modules/dna-polyfills/src/array/is-array.js',
         'node_modules/dna-polyfills/src/dom/class-list.js',
         'node_modules/dna-polyfills/src/object/set-prototype-of.js',
+        'node_modules/dna-polyfills/src/function/name.js',
         'node_modules/dna-polyfills/src/extra/custom-elements.js',
         'node_modules/virtual-dom/dist/virtual-dom.js'
     );
