e98d97cde847c1d357e9720748e52f1f519d7b4e
Edoardo Cavazza <edoardo.cavazza@chialab.it>
2016-07-12 14:46:04 +0200

    feat: handle properties in DNA React library

--- src/plugins/dna.react.js
+++ src/plugins/dna.react.js
@@ -1,4 +1,4 @@
-/* global React */
+/* global React ReactDOM */
 import { digest } from '../helpers/digest.js';
 import { dashToCamel } from '../helpers/strings.js';
 import { register as _register } from './dna.vdom-elements.js';
@@ -6,31 +6,47 @@ import { register as _register } from './dna.vdom-elements.js';
 // export library
 export * from './dna.vdom-elements.js';
 
+/**
+ * Create a React component wrapper.
+ * @private
+ *
+ * @param {string} tagName The tag to use for the custom element. (required)
+ * @param {function} Scope The component definition.
+ * @param {object} config A configuration object. (`prototype` key is required)
+ * @return {function} The React Component constructor.
+ */
 function createClass(tagName, Scope, config = {}) {
-    let props = (Scope.observedProperties || [])
-        .concat(Scope.observedAttributes || [])
-        .map(dashToCamel);
+    let props = (Scope.observedProperties || []).map(dashToCamel);
+    let attrs = (Scope.observedAttributes || []).map(dashToCamel);
     return class extends React.Component {
         static get getDefaultProps() {
-            let attrs = {};
-            props.forEach((name) => {
-                attrs[name] = undefined;
+            let reactProps = {};
+            props.concat(attrs).forEach((name) => {
+                reactProps[name] = undefined;
             });
-            return attrs;
+            return reactProps;
         }
 
         render() {
-            let attrs = {};
-            props.forEach((name) => {
-                attrs[name] = this.props[name];
+            let nodeAttrs = {};
+            attrs.forEach((name) => {
+                nodeAttrs[name] = this.props[name];
             });
             if (config.extends) {
-                attrs.is = tagName;
+                nodeAttrs.is = tagName;
             }
-            return React.createElement(
+            let node = React.createElement(
                 config.extends || tagName,
-                attrs
+                nodeAttrs
             );
+            return node;
+        }
+
+        componentDidMount() {
+            let node = ReactDOM.findDOMNode(this);
+            props.forEach((name) => {
+                node[name] = this.props[name];
+            });
         }
     };
 }
