754dfe4f65298c1ade3e132bc4e13d1f71342180
Edoardo Cavazza <edoardo.cavazza@chialab.it>
2016-09-29 16:39:18 +0200

    refactor: remove Element.prototype.matches polyfill

--- src/helpers/delegate.js
+++ src/helpers/delegate.js
@@ -1,10 +1,8 @@
-import { matches } from './matches.js';
-
 export function delegate(element, evName, selector, callback) {
     element.addEventListener(evName, (event) => {
         let target = event.target;
         while (target && target !== element) {
-            if (matches(target, selector)) {
+            if (target.matches(selector)) {
                 if (callback.call(element, event, target) === false) {
                     return;
                 }
