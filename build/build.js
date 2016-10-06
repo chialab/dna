c8fe85dbfc699cd457b0296e46c93e511564c12a
Edoardo Cavazza <edoardo.cavazza@chialab.it>
2016-10-06 14:50:46 +0200

    chore: new build system

--- build/build.js
+++ build/build.js
@@ -1,40 +1,49 @@
 'use strict';
 
+const rollup = require('rollup').rollup;
+const babel = require('rollup-plugin-babel');
+const uglify = require('rollup-plugin-uglify');
+const minify = require('uglify-js').minify;
 const fs = require('fs');
-const path = require('path');
-const myBuilder = require('../node_modules/es6-workflow/lib/builder.js');
 
-let cwd = process.cwd();
 let packageJSON = require('../package.json');
 let version = packageJSON.version;
 
-packageJSON.build = {
-    globalName: 'DNA',
-    rollup: true,
-    sourceMaps: false,
-    dest: 'dist',
-    globalDeps: {
-        'google/incremental-dom': 'IncrementalDOM',
-    },
-};
-
-function build(file) {
-    let base = path.basename(file);
-    packageJSON.build.main = file;
-    packageJSON.build.bundleName = base;
-    return myBuilder.run(packageJSON).then(() => {
-        let code = fs.readFileSync(path.join(cwd, `dist/${base}`), 'utf8');
-        code = code.replace('self.__DNA__VERSION__', `'${version}'`);
-        fs.writeFileSync(path.join(cwd, `dist/${base}`), code);
+rollup({
+    entry: 'src/dna.js',
+    plugins: [
+        babel({
+            presets: 'es2015-rollup',
+            compact: true,
+            plugins: [
+                ['transform-es2015-classes', {
+                    loose: true,
+                }],
+            ],
+        }),
+        uglify({}, minify),
+    ],
+    external: [],
+}).then((bundle) => {
+    let result = bundle.generate({
+        sourceMap: true,
+        sourceMapFile: 'dna.map',
+        moduleName: 'DNA',
+        format: 'umd',
+        globals: {
+            'google/incremental-dom': 'IncrementalDOM',
+            'skin-template/src/template.js': 'Skin',
+        },
     });
-}
-
-Promise.all([
-    'src/dna.js',
-].map(build)).then(() => {
-    // eslint-disable-next-line
-    console.log('DNA generated!');
-}, (err) => {
-    // eslint-disable-next-line
-    console.error(err);
+    fs.stat('dist', (err) => {
+        if (err) {
+            fs.mkdirSync('dist');
+        }
+        result.code = result.code.replace('self.__DNA__VERSION__', `'${version}'`);
+        fs.writeFileSync('dist/dna.js', `${result.code}
+//# sourceMappingURL=dna.js.map`);
+        fs.writeFileSync('dist/dna.js.map', result.map.toString());
+    });
+}).catch((err) => {
+    console.log(err);
 });
