9016ba053af853a77101da159bf8d34fb3de7efd
Edoardo Cavazza <edoardo.cavazza@chialab.it>
2016-10-18 18:56:23 +0200

    test: add IE9 and IE10 to CI tests

--- sc.browsers.js
+++ sc.browsers.js
@@ -81,6 +81,18 @@ module.exports = {
         platform: 'OS X 10.11',
         version: '9',
     },
+    SL_IE_09: {
+        base: 'SauceLabs',
+        browserName: 'internet explorer',
+        platform: 'Windows 7',
+        version: '9',
+    },
+    SL_IE_10: {
+        base: 'SauceLabs',
+        browserName: 'internet explorer',
+        platform: 'Windows 7',
+        version: '10',
+    },
     SL_IE_11: {
         base: 'SauceLabs',
         browserName: 'internet explorer',
