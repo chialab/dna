b9deb47ac97ea5a9a9cbab8e922d3448d5712a4a
Edoardo Cavazza <edoardo.cavazza@chialab.it>
2016-10-27 15:25:19 +0200

    feat: add attribute methods to nodes lib

--- src/lib/nodes.js
+++ src/lib/nodes.js
@@ -10,24 +10,24 @@ export function getComponent(node) {
         let is = node.getAttribute('is') || node.tagName;
         return registry.get(is);
     }
-    return null;
+    return registry.get(node);
 }
 
 export function connect(node) {
     if (isFunction(node[CONNECTED])) {
-        node[CONNECTED]();
+        node[CONNECTED].call(node);
     }
 }
 
 export function disconnect(node) {
     if (isFunction(node[DISCONNECTED])) {
-        node[DISCONNECTED]();
+        node[DISCONNECTED].call(node);
     }
 }
 
 export function update(node, name, oldValue, newValue) {
     if (isFunction(node[UPDATED]) && name !== 'is') {
-        node[UPDATED](name, oldValue, newValue);
+        node[UPDATED].call(node, name, oldValue, newValue);
     }
 }
 
@@ -49,6 +49,13 @@ export function create(node, descriptor) {
     }
 }
 
+export function createElement(is) {
+    let descriptor = getComponent(is);
+    if (descriptor) {
+        return new descriptor.Ctr();
+    }
+}
+
 export function appendChild(parent, node) {
     if (parent !== node.parentNode || parent.lastElementChild !== node) {
         if (node.parentNode) {
@@ -62,4 +69,22 @@ export function appendChild(parent, node) {
 export function removeChild(parent, node) {
     parent.removeChild(node);
     disconnect(node);
+}
+
+export function setAttribute(node, name, value) {
+    let oldValue = node.getAttribute(name);
+    node.setAttribute(name, value);
+    let attrs = node.constructor.observedAttributes || [];
+    if (attrs.indexOf(name) !== -1) {
+        update(node, name, oldValue, value);
+    }
+}
+
+export function removeAttribute(node, name) {
+    let oldValue = node.getAttribute(name);
+    node.removeAttribute(name);
+    let attrs = node.constructor.observedAttributes || [];
+    if (attrs.indexOf(name) !== -1) {
+        update(node, name, oldValue, null);
+    }
 }
