69c80958d1738cd35e7dd5196499bf48ad5a4482
Edoardo Cavazza <edoardo.cavazza@chialab.it>
2016-11-11 15:00:35 +0100

    feat: add dna-react package

---
+++ packages/dna-react/index.js
@@ -0,0 +1,41 @@
+/**
+ * DNA
+ * (c) 2015-2016 Chialab (http://www.chialab.com) <dev@chialab.io>
+ * http://dna.chialab.io
+ *
+ * Just another components pattern.
+ * Use with React.
+ */
+import ReactDOM from 'react-dom';
+import { mix, MIXINS } from '@dnajs/core/src/library-helpers.js';
+import { registry } from '@dnajs/core/src/lib/registry.js';
+import { BaseComponent as OriginalComponent } from '@dnajs/core';
+import { ReactMixin } from './src/mixins/react.js';
+
+MIXINS.ReactMixin = ReactMixin;
+
+export { registry };
+export * from '@dnajs/core/src/library-helpers.js';
+
+export function defineComponent(tagName, Component, config) {
+    Object.defineProperty(Component.prototype, 'is', {
+        get: () => tagName,
+    });
+    registry.define(tagName, Component, config);
+    return function(...args) {
+        return new Component(...args);
+    };
+}
+
+export function render(parent, Component) {
+    ReactDOM.render(
+        new Component().render(),
+        parent
+    );
+}
+
+export class BaseComponent extends mix(
+    OriginalComponent
+).with(
+    ReactMixin
+) {}
---
+++ packages/dna-react/src/mixins/react.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { DOM } from '@dnajs/core/src/library-helpers.js';
+
+export const ReactMixin = (superClass) => {
+    let ReactComponent = class extends React.Component {
+        render() {
+            return React.createElement(
+                this.is
+            );
+        }
+
+        componentDidMount() {
+            let node = ReactDOM.findDOMNode(this);
+            if (node) {
+                if (!node.reactComponent) {
+                    DOM.bind(node);
+                    node.reactComponent = this;
+                }
+                if (DOM.isComponent(node)) {
+                    DOM.connect(node);
+                    for (let k in this.props) {
+                        node[k] = this.props[k];
+                    }
+                }
+            }
+        }
+
+        componentWillUnmount() {
+            let node = ReactDOM.findDOMNode(this);
+            if (node) {
+                if (DOM.isComponent(node)) {
+                    DOM.disconnect(node);
+                }
+            }
+        }
+
+        componentDidUpdate() {
+            let node = ReactDOM.findDOMNode(this);
+            if (node) {
+                for (let k in this.props) {
+                    node[k] = this.props[k];
+                }
+            }
+        }
+    };
+
+    return class extends superClass {
+        constructor(...args) {
+            super(...args);
+            if (args.length !== 0) {
+                let cmp = new ReactComponent(...args);
+                cmp.is = this.is;
+                return cmp;
+            }
+        }
+    };
+};
