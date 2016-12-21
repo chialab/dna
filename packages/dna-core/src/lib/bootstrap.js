7775468608596d438ca4a12852c21e46cabfb067
Edoardo Cavazza <edoardo.cavazza@chialab.it>
2016-12-21 14:54:37 +0100

    docs: add documentation for bootstrap method

--- packages/dna-core/src/lib/bootstrap.js
+++ packages/dna-core/src/lib/bootstrap.js
@@ -1,10 +1,14 @@
 import { registry } from './registry.js';
 import { connect } from './dom.js';
 
-export function bootstrap(parent) {
+/**
+ * Instantiate all defined components in a DOM tree.
+ * @param {HTMLElement} root The root Node of the tree.
+ */
+export function bootstrap(root) {
     for (let k in registry.components) {
         let Component = registry.get(k);
-        let elements = parent.querySelectorAll(k);
+        let elements = root.querySelectorAll(k);
         for (let i = 0, len = elements.length; i < len; i++) {
             let component = new Component();
             component.node = elements[i];
