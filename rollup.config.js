ea05bb481a306b5ce73a5cb90bffbcf557793c26
Edoardo Cavazza <edoardo.cavazza@chialab.it>
2017-03-08 11:00:04 +0100

    chore: trying to fix ios 9 tests

--- rollup.config.js
+++ rollup.config.js
@@ -12,13 +12,19 @@ module.exports = {
     moduleName: 'DNA',
     plugins: [
         alias({
+            // packages
             '@dnajs/core': path.resolve('./packages/dna-core'),
             '@dnajs/custom-elements-v0': path.resolve('./packages/dna-custom-elements-v0'),
             '@dnajs/custom-elements-v1': path.resolve('./packages/dna-custom-elements-v1'),
             '@dnajs/idom': path.resolve('./packages/dna-idom'),
             '@dnajs/react': path.resolve('./packages/dna-react'),
+            // dev
+            'react-dom': path.resolve('./node_modules/react-dom/dist/react-dom.min.js'),
+            'react': path.resolve('./node_modules/react/dist/react.min.js'),
         }),
-        resolve(),
+        resolve({
+            skip: ['react', 'react-dom'],
+        }),
         replace({
             'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
         }),
