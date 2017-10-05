a28468fb0ab72105a87d5c10ec4adb6761f6d0e2
Edoardo Cavazza <edoardo.cavazza@chialab.it>
2017-10-05 15:07:10 +0200

    refactor: do not use style sheets for shadow scoping

--- packages/core/src/lib/shadow-css.js
+++ packages/core/src/lib/shadow-css.js
@@ -5,81 +5,45 @@
  */
 const HOST_REGEX = /:host(\(([^({)]+(\([^)]*\))?)+\))?/g;
 /**
- * A regex to match content rule.
+ * A regex to match css comments.
  * @type {RegExp}
  * @private
  */
-const CONTENT_REGEX = /(?:^|\s)content:\s*([\w_-]*);/g;
-
+const CSS_COMMENTS_REGEX = /\s*(?!<")\/\*[^*]+\*\/(?!")\s*/g;
 /**
- * CSSKeyframesRule class.
- * @type {function}
+ * A regex to match css rules.
+ * @type {RegExp}
  * @private
  */
-const KFRule = self.CSSKeyframesRule ||
-    self.WebKitCSSKeyframesRule ||
-    self.MozCSSKeyframesRule;
-
-/**
- * Add a scope to all selectors.
- * @private
- *
- * @param {CSSStyleSheet} sheet The css sheet to scope.
- * @param {String} scope The scope selector.
- */
-function scoped(sheet, scope) {
-    let rules = sheet.cssRules || sheet.rules;
-    if (sheet.insertRule) {
-        let reg = new RegExp(`${scope}([\\s.[:]|$)`);
-        for (let i = 0, len = rules.length; i < len; i++) {
-            let rule = rules[i];
-            if (!(rule instanceof KFRule)) {
-                try {
-                    let body = rule.cssText;
-                    if (rule.selectorText) {
-                        let selector = rule.cssText.split('{').shift().split(',')
-                            .map((rule) => {
-                                rule = rule.trim();
-                                if (rule.match(reg)) {
-                                    return rule;
-                                }
-                                return `${scope} ${rule}`;
-                            })
-                            .join(', ');
-                        body = rule.cssText
-                            .replace(rule.selectorText, selector)
-                            .replace(CONTENT_REGEX, 'content: "$1";');
-                    } else if (rule.cssRules || rule.rules) {
-                        scoped(rule, scope);
-                        body = rule.cssText;
-                    }
-                    sheet.deleteRule(i);
-                    sheet.insertRule(body, i);
-                } catch (ex) {
-                    //
-                }
-            }
-        }
-    }
-    return [].map.call(
-        rules,
-        (rule) => rule.cssText.replace(CONTENT_REGEX, 'content: "$1";')
-    ).join('');
-}
+const CSS_RULES_REGEX = /(#|\.|@|\[|[a-zA-Z])([^{;}]*){/g;
 
 /**
  * Convert a shadowDOM css string into a normal scoped css.
  * @private
  *
- * @param {HTMLStyleElement} style The style element.
  * @param {String} css The css string to convert.
  * @param {String} is The component name for scoping.
+ * @return {String} The scoped css.
  */
-export function convertShadowCSS(style, css, is) {
+export function convertShadowCSS(css, is) {
     let scope = `.${is}`;
-    style.textContent = css
-        .replace(HOST_REGEX, (fullMatch, mod) =>
-            `${scope}${mod ? mod.slice(1, -1) : ''}`
-        );
-    style.textContent = scoped(style.sheet, scope);
+    return css
+        .replace(CSS_COMMENTS_REGEX, '')
+        .replace(HOST_REGEX, (fullMatch, mod) => `${scope}${mod ? mod.slice(1, -1) : ''}`)
+        .replace(CSS_RULES_REGEX, (match) => {
+            match = match.trim();
+            if (match[0] === '@') {
+                return match;
+            }
+            return match
+                .split(',')
+                .map((selector) => {
+                    selector = selector.trim();
+                    if (selector.indexOf(scope) === 0) {
+                        return selector;
+                    }
+                    return `${scope} ${selector}`;
+                })
+                .join(',');
+        });
 }
