e27227f0f8a678669962641830296cd7a247a06f
batopa <batopa@gmail.com>
2017-11-28 09:47:29 +0100

    feat: add callback before connect components

--- packages/core/src/lib/bootstrap.js
+++ packages/core/src/lib/bootstrap.js
@@ -6,14 +6,19 @@ import { getNodeComponent, connect } from './dom.js';
  * @memberof DNA
  *
  * @param {HTMLElement} root The root Node of the tree.
+ * @param {Function} beforeConnectCallback A callback called before connecting every component
+ * @return {void}
  */
-export function bootstrap(root) {
+export function bootstrap(root, beforeConnectCallback) {
     for (let k in registry.components) {
         let Component = registry.get(k);
         let elements = root.querySelectorAll(`${k}, [is="${k}"]`);
         for (let i = 0, len = elements.length; i < len; i++) {
             if (!getNodeComponent(elements[i])) {
                 let component = new Component(elements[i]);
+                if (beforeConnectCallback && typeof beforeConnectCallback === 'function') {
+                    beforeConnectCallback(component, elements[i]);
+                }
                 connect(component);
             }
         }
