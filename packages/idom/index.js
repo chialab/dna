14979efad261d9c3667cd7db73c21b6af18565b5
Edoardo Cavazza <edoardo.cavazza@chialab.it>
2018-12-07 09:39:09 +0100

    #48 fix: handle native node extension in IDOM implementation

--- packages/idom/index.js
+++ packages/idom/index.js
@@ -11,12 +11,13 @@ import * as IDOM from './src/lib/idom.js';
 import { IDOMMixin } from './src/mixins/idom.js';
 import { mix, MIXINS } from '@dnajs/core/src/core.js';
 import { proxy } from '@dnajs/core/src/lib/proxy.js';
+import { registry } from '@dnajs/core/src/lib/registry.js';
 
 MIXINS.IDOMMixin = IDOMMixin;
 
 export * from '@dnajs/core/src/core.js';
 export { proxy };
-export { registry } from '@dnajs/core/src/lib/registry.js';
+export { registry };
 export { bootstrap } from '@dnajs/core/src/lib/bootstrap.js';
 export { define } from '@dnajs/core/src/lib/define.js';
 export { render } from '@dnajs/core/src/lib/render.js';
@@ -25,7 +26,17 @@ export const h = IDOM.h;
 
 const Component = proxy(class {
     constructor(node) {
-        this.node = node || document.createElement(this.is);
+        if (!node) {
+            let desc = registry.get(this.is, true);
+            let config = desc.config;
+            node = document.createElement(
+                config.extends ? config.extends : desc.is
+            );
+            if (config.extends) {
+                node.setAttribute('is', desc.is);
+            }
+        }
+        this.node = node;
     }
 });
 
