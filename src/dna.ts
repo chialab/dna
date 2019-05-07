65927b4e2caa152dcbac333f4d385af3f6f0112a
Edoardo Cavazza <edoardo.cavazza@chialab.it>
2019-05-07 15:55:11 +0200

    #54 feat: handle JSX Fragment

--- src/dna.ts
+++ src/dna.ts
@@ -1,7 +1,7 @@
 export { DOM } from './lib/dom';
 export { render } from './lib/render';
 export { define } from './lib/registry';
-export { h } from './lib/h';
+export { Fragment, h } from './lib/h';
 export { html } from './lib/html';
 export { css } from './lib/css';
 export { interpolate } from './lib/interpolate';
