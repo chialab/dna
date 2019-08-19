76513f14733382e653979f4dccfe4c12c4950ebc
Edoardo Cavazza <edoardo.cavazza@chialab.it>
2019-08-19 16:57:06 +0200

    chore: move docs to wiki

--- docs/_Header_.md
+++
@@ -1,5 +0,0 @@
-<img alt="DNA" src="https://logos.chialab.io/@chialab/dna.svg" width="100">
-
-# DNA
-
-## Progressive Web Components
--- scripts/website/assets/script.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener('load', function() {
-    var href = '.' + this.location.pathname;
-    var element = document.querySelector('nav li a[href="' + href + '"]');
-    if (element) {
-        element.classList.add('active');
-    }
-});
