f079609e73e45c07d6e61afbe408dfabef95f1e5
Edoardo Cavazza <edoardo.cavazza@chialab.it>
2020-03-24 08:31:57 +0100

    chore: fix adpater links

--- src/dna.ts
+++ src/dna.ts
@@ -1,18 +1,20 @@
 import { registry } from './lib/CustomElementRegistry';
 
+export { CustomElement } from './lib/CustomElement';
+export { CustomElementRegistry } from './lib/CustomElementRegistry';
 export { window } from './lib/window';
 export { registry as customElements };
 export const get = registry.get.bind(registry);
 export const define = registry.define.bind(registry);
 export const upgrade = registry.upgrade.bind(registry);
 export const whenDefined = registry.whenDefined.bind(registry);
-export { connect, disconnect, DOM } from './lib/DOM';
-export { CustomElementRegistry } from './lib/CustomElementRegistry';
+export { TemplateItem, TemplateItems, Template, TemplateFilter } from './lib/Template';
+export { DOM, connect, disconnect } from './lib/DOM';
 export { render } from './lib/render';
 export { Fragment } from './lib/Fragment';
-export { h } from './lib/HyperNode';
+export { HyperNode, h } from './lib/HyperNode';
 export { html, template, interpolate } from './lib/html';
 export { css } from './lib/css';
-export { AsyncEvent, delegateEventListener, undelegateEventListener, dispatchEvent, dispatchAsyncEvent } from './lib/events';
-export { property } from './lib/property';
+export { AsyncEvent, DelegatedEventCallback, delegateEventListener, undelegateEventListener, dispatchEvent, dispatchAsyncEvent } from './lib/events';
+export { ClassFieldDescriptor, ClassFieldObserver, ClassFieldValidator, ClassFieldAttributeConverter, ClassFieldPropertyConverter, property } from './lib/property';
 export { extend, Component, Properties } from './lib/Component';
