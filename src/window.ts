2e951d925c79ccf1226c55ce4c146097501b6243
Edoardo Cavazza <edoardo.cavazza@chialab.it>
2020-12-14 18:46:23 +0100

    fix: skip jsdom bundling

--- src/window.ts
+++ src/window.ts
@@ -1,3 +1,13 @@
-// eslint-disable-next-line
-const namespace = typeof window !== 'undefined' ? window : (new (require('jsdom').JSDOM)()).window as Window & typeof globalThis;
+let namespace: Window & typeof globalThis;
+if (typeof window !== 'undefined') {
+    namespace = window;
+} else {
+    // skip jsdom bundling
+    /* eslint-disable quotes */
+    const require = module[`require`].bind(module);
+    /* eslint-disable-next-line */
+    let jsdom = require('jsdom');
+    namespace = new jsdom.JSDOM().window as Window & typeof globalThis;
+}
+
 export { namespace as window };
