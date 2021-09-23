09d78a01e9f3ff2252a837a5adb0cc703f97e543
Edoardo Cavazza <edoardo.cavazza@chialab.it>
2021-09-23 11:50:36 +0200

    refactor: partially handle unhandled platforms

--- src/window.ts
+++ src/window.ts
@@ -2,9 +2,30 @@ import jsdom from 'jsdom';
 
 let namespace: Window & typeof globalThis;
 if (typeof window !== 'undefined') {
+    // browser
     namespace = window;
-} else {
+} else if (typeof global !== 'undefined') {
+    // node
     namespace = new jsdom.JSDOM().window as unknown as Window & typeof globalThis;
+} else {
+    // ¯\_(ツ)_/¯
+    const throwPlatform = function() {
+        throw new Error('Missing DOM implementation.');
+    };
+    namespace = {
+        Node: throwPlatform,
+        HTMLElement: throwPlatform,
+        Event: throwPlatform,
+        CustomEvent: throwPlatform,
+        document: {
+            createDocumentFragment: throwPlatform,
+            createElement: throwPlatform,
+            createElementNS: throwPlatform,
+            createTextNode: throwPlatform,
+            createComment: throwPlatform,
+            addEventListener() { },
+        },
+    } as unknown as Window & typeof globalThis;
 }
 
 export { namespace as window };
