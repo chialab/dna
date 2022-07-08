12d91617805ef9104422f2c8640efb5c0d3b2575
Edoardo Cavazza <edoardo.cavazza@chialab.it>
2022-07-08 08:52:30 +0200

    chore: add integrations tests and docs

--- docs/integrations.md
+++ docs/integrations.md
@@ -1,6 +1,43 @@
-# Manipulating the DOM
+# Integrations
 
-## Why and how
+## Frameworks
+
+DNA uses its own render to handle slotted nodes in the light DOM. So, we run tests for integration with other frontend frameworks like React, Angular, Lit and Vue. Here is a list of passing tests and known issues:
+
+| Framwework | Text | Elements | Named slots |
+|------------|:----:|:--------:|:-----------:|
+| React      | ✅   | ✅        | ✅          |
+| Lit        | ✅   | ✅        | ❌          |
+| Vue        | 🚧   | 🚧        | 🚧          |
+| Angular    | 🚧   | 🚧        | 🚧          |
+
+## View libraries
+
+Sometimes you need to encapsulate in DNA another UI library, like [Mapbox](https://github.com/mapbox/mapbox-gl-js) or [Pickr](https://github.com/Simonwep/pickr). Since DNA components are DOM nodes, the integration is possible using the element context as library param:
+
+```ts
+import { Component, customElement } from '@chialab/dna';
+import Pickr from '@simonwep/pickr';
+
+@customElement('color-picker')
+export class ColorPicker extends Component {
+    private pickr: Pickr;
+
+    connectedCallback() {
+        super.connectedCallback();
+        this.pickr = new Pickr({
+            el: this,
+        });
+    }
+
+    disconnectedCallback() {
+        this.pickr.destroyAndRemove();
+        super.disconnectedCallback();
+    }
+}
+```
+
+## Manipulating the DOM
 
 Since DNA does not require any Custom Elements polyfill, the [life cycle](./life-cycle) is delegated to the render cycle which uses the `DOM` helper under the hood. This helper invokes the life cycle methods for each DOM operation like `appendChild`, `removeChild` etc.
 If you want to manipulate the DOM tree outside of a render cycle, always use `DOM` methods instead of the `HTMLElement` prototype:
@@ -33,48 +70,3 @@ All methods inherit the prototype signature with the context node as first argum
 | `matches(selector)` | `matches(element, selector)` |
 
 </aside>
-
-## Setting `innerHTML`
-
-Since DNA render cycle is responsible for Custom Element's life cycle support in old browser, directly setting the `innerHTML` to an element can lead to unexpected behavior. You should always consider to use the `html` helper coombine with the `render` method, but when it is not possible you can always invoke the `customElements.upgrade` method to the changed root node:
-
-```ts
-import { customElements } from '@chialab/dna';
-import './video-player.js';
-
-const element = document.querySelector('main');
-element.innerHTML = `<article>
-    <h1>Title</h1>
-    <video-player>
-        <source src="/video.mp4" />
-    </video-player>
-</article>`;
-
-customElements.upgrade(element);
-```
-
-## Integration with third party libraries
-
-Sometimes you need to encapsulate in DNA another UI library, like [Mapbox](https://github.com/mapbox/mapbox-gl-js) or [Pickr](https://github.com/Simonwep/pickr). Since DNA components are DOM nodes, the integration is possible using the element context as library param:
-
-```ts
-import { Component, customElement } from '@chialab/dna';
-import Pickr from '@simonwep/pickr';
-
-@customElement('color-picker')
-export class ColorPicker extends Component {
-    private pickr: Pickr;
-
-    connectedCallback() {
-        super.connectedCallback();
-        this.pickr = new Pickr({
-            el: this,
-        });
-    }
-
-    disconnectedCallback() {
-        this.pickr.destroyAndRemove();
-        super.disconnectedCallback();
-    }
-}
-```
---
+++ test/integrations.spec.js
@@ -0,0 +1,185 @@
+// eslint-disable-next-line import/no-unresolved
+import { expect } from '@chialab/ginsenghino';
+import * as DNA from '@chialab/dna';
+
+describe('Integrations', () => {
+    let TestElement, wrapper;
+    before(() => {
+        TestElement = class extends DNA.Component {
+            static get properties() {
+                return {
+                    prop: { type: String },
+                };
+            }
+
+            render() {
+                return DNA.html`
+                    <span>
+                        <slot />
+                    </span>
+                    <div>
+                        <slot name="children" />
+                    </div>
+                    ${DNA.html`<p>${this.prop}</p>`}
+                `;
+            }
+        };
+
+        DNA.customElements.define('test-element-integration', TestElement);
+    });
+
+    beforeEach(() => {
+        wrapper = DNA.DOM.createElement('div');
+        DNA.DOM.appendChild(DNA.document.body, wrapper);
+    });
+
+    afterEach(() => {
+        DNA.DOM.removeChild(DNA.document.body, wrapper);
+    });
+
+    describe('react', () => {
+        let React, ReactDOM;
+        before(async () => {
+            React = await import('react');
+            ReactDOM = await import('react-dom');
+        });
+
+        it('should update text content', async () => {
+            const element = ReactDOM.render(React.createElement('test-element-integration', {}, ['Text']), wrapper);
+            expect(element.children[0].tagName).to.be.equal('SPAN');
+            expect(element.children[0].textContent).to.be.equal('Text');
+            expect(element.children[1].tagName).to.be.equal('DIV');
+
+            ReactDOM.render(React.createElement('test-element-integration', {}, ['Text update']), wrapper);
+            expect(wrapper.children[0]).to.be.equal(element);
+            expect(element.children[0].tagName).to.be.equal('SPAN');
+            expect(element.children[0].textContent).to.be.equal('Text update');
+        });
+
+        it('should update text content with multiple text nodes', async () => {
+            const element = ReactDOM.render(React.createElement('test-element-integration', {}, ['Text', ' ', 'children']), wrapper);
+            expect(element.children[0].tagName).to.be.equal('SPAN');
+            expect(element.children[0].textContent).to.be.equal('Text children');
+            expect(element.children[1].tagName).to.be.equal('DIV');
+
+            ReactDOM.render(React.createElement('test-element-integration', {}, ['Update', ' ', 'children']), wrapper);
+            expect(element.children[0].tagName).to.be.equal('SPAN');
+            expect(element.children[0].textContent).to.be.equal('Update children');
+        });
+
+        it('should update named slots', () => {
+            const element = ReactDOM.render(React.createElement('test-element-integration', {}, ['Text', React.createElement('h1', { slot: 'children' }, 'Title')]), wrapper);
+            expect(element.children[0].tagName).to.be.equal('SPAN');
+            expect(element.children[0].textContent).to.be.equal('Text');
+            expect(element.children[1].tagName).to.be.equal('DIV');
+            expect(element.children[1].children).to.have.lengthOf(1);
+            expect(element.children[1].children[0].tagName).to.be.equal('H1');
+            expect(element.children[1].children[0].textContent).to.be.equal('Title');
+
+            ReactDOM.render(React.createElement('test-element-integration', {}, ['Text', React.createElement('h2', { slot: 'children' }, 'Subtitle')]), wrapper);
+            expect(element.children[0].tagName).to.be.equal('SPAN');
+            expect(element.children[0].textContent).to.be.equal('Text');
+            expect(element.children[1].tagName).to.be.equal('DIV');
+            expect(element.children[1].children).to.have.lengthOf(1);
+            expect(element.children[1].children[0].tagName).to.be.equal('H2');
+            expect(element.children[1].children[0].textContent).to.be.equal('Subtitle');
+        });
+
+        it('should update a property', () => {
+            const element = ReactDOM.render(React.createElement('test-element-integration', { prop: 'value' }), wrapper);
+            expect(element.prop).to.be.equal('value');
+            expect(element.children[2].tagName).to.be.equal('P');
+            expect(element.children[2].textContent).to.be.equal('value');
+
+            ReactDOM.render(React.createElement('test-element-integration', { prop: 'value update' }), wrapper);
+            expect(element.prop).to.be.equal('value update');
+            expect(element.children[2].tagName).to.be.equal('P');
+            expect(element.children[2].textContent).to.be.equal('value update');
+        });
+    });
+
+    describe('lit', () => {
+        let lit;
+        before(async () => {
+            lit = await import('lit');
+        });
+
+        it('should update text content', async () => {
+            const Template = (text) => lit.html`<test-element-integration>${text}</test-element-integration>`;
+
+            lit.render(Template('Text'), wrapper);
+            const element = wrapper.children[0];
+
+            // element.forceUpdate();
+            expect(element.children[0].tagName).to.be.equal('SPAN');
+            expect(element.children[0].textContent).to.be.equal('Text');
+            expect(element.children[1].tagName).to.be.equal('DIV');
+
+            lit.render(Template('Text update'), wrapper);
+            expect(wrapper.children[0]).to.be.equal(element);
+            expect(element.children[0].tagName).to.be.equal('SPAN');
+            expect(element.children[0].textContent).to.be.equal('Text update');
+        });
+
+        it('should update text content with multiple text nodes', async () => {
+            const Template = (text) => lit.html`<test-element-integration>${text} children</test-element-integration>`;
+
+            lit.render(Template('Text'), wrapper);
+            const element = wrapper.children[0];
+
+            expect(element.children[0].tagName).to.be.equal('SPAN');
+            expect(element.children[0].textContent).to.be.equal('Text children');
+            expect(element.children[1].tagName).to.be.equal('DIV');
+
+            lit.render(Template('Update'), wrapper);
+            expect(element.children[0].tagName).to.be.equal('SPAN');
+            expect(element.children[0].textContent).to.be.equal('Update children');
+        });
+
+        it('should update named slots', () => {
+            const Template = (title) => lit.html`<test-element-integration>
+                Text
+                ${title ? lit.html`<h1 slot="children">Title</h1>` : lit.html`<h2 slot="children">Subtitle</h2>`}
+            </test-element-integration>`;
+            lit.render(Template(true), wrapper);
+            const element = wrapper.children[0];
+
+            expect(element.children[0].tagName).to.be.equal('SPAN');
+            expect(element.children[0].textContent.trim()).to.be.equal('Text');
+            expect(element.children[1].tagName).to.be.equal('DIV');
+            expect(element.children[1].children).to.have.lengthOf(1);
+            expect(element.children[1].children[0].tagName).to.be.equal('H1');
+            expect(element.children[1].children[0].textContent.trim()).to.be.equal('Title');
+
+            lit.render(Template(false), wrapper);
+            expect(element.children[0].tagName).to.be.equal('SPAN');
+            expect(element.children[0].textContent.trim()).to.be.equal('Text');
+            expect(element.children[1].tagName).to.be.equal('DIV');
+            expect(element.children[1].children).to.have.lengthOf(1);
+            expect(element.children[1].children[0].tagName).to.be.equal('H2');
+            expect(element.children[1].children[0].textContent.trim()).to.be.equal('Subtitle');
+        });
+
+        it('should update a property', () => {
+            const Template = (value) => lit.html`<test-element-integration prop=${value}></test-element-integration>`;
+
+            lit.render(Template('value'), wrapper);
+            const element = wrapper.children[0];
+
+            expect(element.prop).to.be.equal('value');
+            expect(element.children[2].tagName).to.be.equal('P');
+            expect(element.children[2].textContent).to.be.equal('value');
+
+            lit.render(Template('value update'), wrapper);
+            expect(element.prop).to.be.equal('value update');
+            expect(element.children[2].tagName).to.be.equal('P');
+            expect(element.children[2].textContent).to.be.equal('value update');
+        });
+    });
+
+    describe('vue', () => {
+        it.skip('should update text content', () => {
+
+        });
+    });
+});
