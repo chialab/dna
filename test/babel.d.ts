1db2143d363bc0ff58a27e1c859f79d8835042fa
Edoardo Cavazza <edoardo.cavazza@chialab.it>
2025-05-30 09:14:12 +0200

    Migrate some more tests to typescript

--- test/babel.d.ts
+++ test/babel.d.ts
@@ -1,6 +1,23 @@
 declare module '@babel/runtime/helpers/decorate' {
-    // biome-ignore lint/suspicious/noExplicitAny: We dont care about Babel types.
-    function decorate<T>(decorators: any[], factory: (...args: any[]) => any, superClass: T, mixins?: any[]): T;
+    function decorate<
+        S,
+        F extends (
+            initialize: (instance: InstanceType<S>) => void,
+            superClass: S
+        ) => {
+            F: new (
+                // biome-ignore lint/suspicious/noExplicitAny: We dont care about Babel types.
+                ...args: any[]
+            ) => InstanceType<S>;
+        },
+    >(
+        // biome-ignore lint/suspicious/noExplicitAny: We dont care about Babel types.
+        decorators: any[],
+        factory: F,
+        superClass: S,
+        // biome-ignore lint/suspicious/noExplicitAny: We dont care about Babel types.
+        mixins?: any[]
+    ): ReturnType<F>['F'];
 
     export default decorate;
 }
