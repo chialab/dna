0ad3ff300642c1b3c5266b83a38c3942cb3a0d17
Edoardo Cavazza <edoardo.cavazza@chialab.it>
2025-05-30 10:42:43 +0200

    Disable svelte plugins

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { fileURLToPath } from 'node:url';
-import { svelte } from '@sveltejs/vite-plugin-svelte';
-import { svelteTesting } from '@testing-library/svelte/vite';
+// import { svelte } from '@sveltejs/vite-plugin-svelte';
+// import { svelteTesting } from '@testing-library/svelte/vite';
 import { type Plugin, defineConfig } from 'vitest/config';
 
 const job = (() => {
@@ -18,7 +18,7 @@ const job = (() => {
 })();
 
 export default defineConfig({
-    plugins: [svelte() as Plugin[], svelteTesting() as Plugin],
+    // plugins: [svelte() as Plugin[], svelteTesting() as Plugin],
     esbuild: {
         include: /\.(m?(t|j)s|[jt]sx)$/,
         target: ['es2020'],
