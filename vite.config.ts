fab5a689ade5823fb3aeacd7419463e807c49d89
Edoardo Cavazza <edoardo.cavazza@chialab.it>
2025-05-30 10:57:45 +0200

    Try to remove job info

--- vite.config.ts
+++ vite.config.ts
@@ -3,20 +3,6 @@ import { fileURLToPath } from 'node:url';
 // import { svelteTesting } from '@testing-library/svelte/vite';
 import { type Plugin, defineConfig } from 'vitest/config';
 
-const job = (() => {
-    if (process.env.TRAVIS) {
-        return `TRAVIS ${process.env.TRAVIS_BUILD_ID}#${process.env.TRAVIS_BUILD_NUMBER}`;
-    }
-    if (process.env.GITLAB_CI) {
-        return `GITLAB ${process.env.CI_JOB_ID}#${process.env.CI_JOB_NAME}`;
-    }
-    if (process.env.GITHUB_ACTIONS) {
-        return `GITHUB ${process.env.GITHUB_JOB}#${process.env.GITHUB_RUN_NUMBER}`;
-    }
-
-    return null;
-})();
-
 export default defineConfig({
     // plugins: [svelte() as Plugin[], svelteTesting() as Plugin],
     esbuild: {
@@ -32,7 +18,6 @@ export default defineConfig({
         },
     },
     test: {
-        // name: `DNA${job ? ` (${job})` : ''}`,
         dir: './test',
         include: ['./**/*.spec.{js,ts,tsx}'],
         fileParallelism: false,
