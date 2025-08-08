b0a1bc8668281aac702a6a66dd4617d2d5eb2308
Edoardo Cavazza <edoardo.cavazza@chialab.it>
2025-08-08 18:56:30 +0200

    Add fix for safari 15 scoped css test

--- test/css.spec.ts
+++ test/css.spec.ts
@@ -140,6 +140,24 @@ h3 {
   0% { top: 0px; }
   100% { top: 10px; }
 }`,
+                `[\\:scope="test-style"] { color: rgb(95, 158, 160); }
+[\\:scope="test-style"] > * { background-color: rgb(95, 158, 160); }
+[\\:scope="test-style"].test { color: rgb(95, 158, 160); }
+[\\:scope="test-style"].test > * { background-color: rgb(95, 158, 160); }
+[\\:scope="test-style"] h3 { color: blue; }
+[\\:scope="test-style"] #before1::before { content: "Hello"; }
+[\\:scope="test-style"] #before2::before { content: attr(id); }
+[\\:scope="test-style"] #before3::before { content: "Hello world"; }
+[\\:scope="test-style"] #before4::before { content: "attr(id)"; }
+[\\:scope="test-style"] #before5::before { content: "♜"; }
+[\\:scope="test-style"] #before6::before { content: "hello-world"; }
+@media (min-width: 0) {
+  [:scope="test-style"] h1, [:scope="test-style"] h2 { color: inherit; }
+}
+@-webkit-keyframes anim {${' '}
+  0% { top: 0px; }
+  100% { top: 10px; }
+}`,
             ]);
         });
 
