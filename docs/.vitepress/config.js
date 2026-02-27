38f0cf4fe82fa3bd87bd575834b04b678cb2507b
Edoardo Cavazza <edoardo.cavazza@chialab.it>
2026-02-27 10:29:00 +0100

    Add matomo code

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -12,7 +12,22 @@ export default defineConfig({
         plugins: [llmstxt()]
     },
 
-    head: [['link', { rel: 'icon', href: '/dna/favicon.png' }]],
+    head: [
+        ['link', { rel: 'icon', href: '/dna/favicon.png' }],
+        ['script', {}, `var _paq = window._paq = window._paq || [];
+    /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+    _paq.push(["setDomains", ["*.chialab.github.io/dna","*.chialab.github.io/loock","*.chialab.github.io/rna"]]);
+    _paq.push(["disableCookies"]);
+    _paq.push(['trackPageView']);
+    _paq.push(['enableLinkTracking']);
+    (function() {
+        var u="https://analytics.chialab.io/";
+        _paq.push(['setTrackerUrl', u+'matomo.php']);
+        _paq.push(['setSiteId', '2']);
+        var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+        g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+    })();`]
+    ],
 
     themeConfig: {
         logo: '/chialab.svg',
